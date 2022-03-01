ba6fe38f506c671a021155d59b646cd9a6098f59
Nikola Stanin <nikolastanin@gmail.com>
2022-03-01 10:35:26 +0100

    added number boxes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
-const heading = document.getElementById("heading1");
+let numberBox = document.querySelectorAll(".number_box"); 
 
-console.log(heading);
+console.log(numberBox);
+
+const chooseNum = (e) =>{
+    let userNum =  parseInt(e.target.innerHTML);
+
+}
+numberBox.forEach(element => {
+    element.addEventListener("click", chooseNum);
+});
