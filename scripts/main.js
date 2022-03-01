285b23ba1c73c0cf39db4c388dbe00d77e6a93c1
Nikola Stanin <nikolastanin@gmail.com>
2022-03-01 18:52:32 +0100

    basic styling

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,35 @@ let numberBox = document.querySelectorAll(".number_box");
 console.log(numberBox);
 
 const chooseNum = (e) =>{
+        return new Promise ((resolve,reject)=>{
+            let userNum =  parseInt(e.target.innerHTML);
+            let randomNum = Math.floor(Math.random() * 9) + 1;
+            console.log(randomNum);
+           
+            if(userNum === randomNum){
+                e.target.classList.add("success");
+                resolve({points:1, randomNum}
+                );
+            }
+            
+            if(userNum!==randomNum){
+                reject(new Error("wrong"));
+            }
 
-    let userNum =  parseInt(e.target.innerHTML);
-console.log(e.target.classList.add("success"));
+        });
+  
 }
-
+const mainFunc = async (e) =>{
+        try{
+const results = await chooseNum(e);
+console.log(results.randomNum, results.points +"guessed right");
+        }
+        catch(error){
+                    console.log(error);
+        }
+}
 
 //event listeners
 numberBox.forEach(element => {
-    element.addEventListener("click", chooseNum);
+    element.addEventListener("click", mainFunc);
 });
