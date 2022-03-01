99214b788132970ef9cef99e0c82af5b3b756a8a
Nikola Stanin <nikolastanin@gmail.com>
2022-03-01 10:46:21 +0100

    added succes and fail class

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,13 @@ let numberBox = document.querySelectorAll(".number_box");
 console.log(numberBox);
 
 const chooseNum = (e) =>{
-    let userNum =  parseInt(e.target.innerHTML);
 
+    let userNum =  parseInt(e.target.innerHTML);
+console.log(e.target.classList.add("success"));
 }
+
+
+//event listeners
 numberBox.forEach(element => {
     element.addEventListener("click", chooseNum);
 });
